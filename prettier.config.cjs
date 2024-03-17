c6e034e20bd55e6af987f8f681b257ceb17955ad
Han Yeong-woo <han@yeongwoo.dev>
2024-03-17 20:08:30 +0900

    chore: update dependencies

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-const hwyConfig = require('@nix6839/prettier-config');
-
-/** @satisfies {import('prettier').Config} */
-const config = {
-  ...hwyConfig,
-};
-
-module.exports = config;
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+import hwyConfig from '@nix6839/prettier-config'
+
+/** @satisfies {import('prettier').Config} */
+const config = hwyConfig
+
+export default config
